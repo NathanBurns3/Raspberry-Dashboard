e7472222d70a4b8c972b02ca26c66b9916b78924
Nathan Burns <nate10nate@gmail.com>
2024-09-26 14:07:59 -0400

    Refactor PhotosFrame to display fetched photo with custom dimensions and handle case when no photo is available

--- src/ui/photos_frame.py
+++ src/ui/photos_frame.py
@@ -1,12 +1,19 @@
 import tkinter as tk
-from utils.data.fetch_photos import fetch_photos
+from utils.data.fetch_photos import fetch_photo
+from PIL import Image, ImageTk
 
 class PhotosFrame(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
         
-        label = tk.Label(self, text="Photos")
-        label.pack(side="top", fill="x", pady=10)
-        photo = fetch_photos()
-        tk.Label(self, text=photo).pack()
+        photo_path = fetch_photo()
+        if photo_path:
+            image = Image.open(photo_path)
+            image = image.resize((800, 480), Image.ANTIALIAS)
+            photo = ImageTk.PhotoImage(image)
+            photo_label = tk.Label(self, image=photo)
+            photo_label.image = photo
+            photo_label.pack(fill="both", expand=True)
+        else:
+            tk.Label(self, text="No photos available").pack()
