098b94fce6cd15b0b297b4b80ced2926e8ad9075
Nathan Burns <nate10nate@gmail.com>
2024-09-26 13:25:53 -0400

    Refactor QuotesFrame to display quotes and authors with custom fonts and styling

--- src/ui/quotes_frame.py
+++ src/ui/quotes_frame.py
@@ -1,12 +1,28 @@
 import tkinter as tk
+from tkinter import font
 from utils.data.fetch_quotes import fetch_quotes
 
 class QuotesFrame(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
+        self.config(width=800, height=480, bg="sky blue")
         
-        label = tk.Label(self, text="Quotes")
-        label.pack(side="top", fill="x", pady=10)
+        self.quote_font = font.Font(family="Georgia", size=36, slant="italic")
+        self.author_font = font.Font(family="Georgia", size=24, weight="bold")
+        
+        self.create_widgets()
+        
+        self.display_quote()
+
+    def create_widgets(self):
+        self.quote_label = tk.Label(self, font=self.quote_font, wraplength=700, justify="center", bg="sky blue", fg="white")
+        self.quote_label.pack(pady=(75, 10), padx=20, fill="both", expand=True)
+        
+        self.author_label = tk.Label(self, font=self.author_font, bg="sky blue", fg="steel blue")
+        self.author_label.pack(pady=(0, 100))
+
+    def display_quote(self):
         quote = fetch_quotes()
-        tk.Label(self, text=quote).pack()
+        self.quote_label.config(text=quote.quote)
+        self.author_label.config(text=quote.author)
