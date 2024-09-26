1a0b440a8dcb35a0d275c30537d9e4772d7afea7
Nathan Burns <nate10nate@gmail.com>
2024-09-26 14:08:05 -0400

    Refactor fetch_photos.py to fetch a random photo from the slideshow directory

--- src/utils/data/fetch_photos.py
+++ src/utils/data/fetch_photos.py
@@ -1,3 +1,9 @@
-def fetch_photos():
-    # Nathan: update with some sort of api call
-    return
+import os
+import random
+
+def fetch_photo():
+    slideshow_dir = os.path.join(os.path.dirname(__file__), "../../../assets/images/slideshow")
+    images = [os.path.join(slideshow_dir, file) for file in os.listdir(slideshow_dir) if file.lower().endswith((".png", ".jpg", ".jpeg"))]
+    if not images:
+        return None
+    return random.choice(images)
