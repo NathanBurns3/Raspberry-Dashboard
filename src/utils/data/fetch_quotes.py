c5d7c855b3982d6cd30f0512081d85f5cab66091
Nathan Burns <nate10nate@gmail.com>
2024-09-26 13:26:15 -0400

    Refactor fetch_quotes.py to fetch and store quotes from an API

--- src/utils/data/fetch_quotes.py
+++ src/utils/data/fetch_quotes.py
@@ -1,3 +1,20 @@
+import requests
+import json
+from models.quotes import Quote
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 def fetch_quotes():
-    # Nathan: update with some sort of api call
-    return
+    category = 'happiness'
+    api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
+    api_key = os.getenv('QUOTE_API_KEY')
+    response = requests.get(api_url, headers={'X-Api-Key': api_key})
+    if response.status_code == requests.codes.ok:
+        quote_data = json.loads(response.text)
+        item = quote_data[0]
+        quote = Quote(item['quote'], item['author'], item['category'])
+        return quote
+    else:
+        print("Error:", response.status_code, response.text)
