36c2adb5d1acefd7d29c7abe4c2841bff161fd11
Nathan Burns <nate10nate@gmail.com>
2024-10-13 20:56:22 -0400

    Refactor PhotosFrame to dynamically update displayed photo and handle cases when no photo is available

--- src/ui/photos_frame.py
+++ src/ui/photos_frame.py
@@ -6,14 +6,17 @@ class PhotosFrame(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
-        
+        self.photo_label = tk.Label(self)
+        self.photo_label.pack(fill="both", expand=True)
+        self.update_photo()
+
+    def update_photo(self):
         photo_path = fetch_photo()
         if photo_path:
             image = Image.open(photo_path)
             image = image.resize((800, 480), Image.ANTIALIAS)
             photo = ImageTk.PhotoImage(image)
-            photo_label = tk.Label(self, image=photo)
-            photo_label.image = photo
-            photo_label.pack(fill="both", expand=True)
+            self.photo_label.configure(image=photo)
+            self.photo_label.image = photo
         else:
-            tk.Label(self, text="No photos available").pack()
+            self.photo_label.configure(text="No photos available")
