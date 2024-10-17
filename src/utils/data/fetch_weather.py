a357fc9292fda0167049d9800f0b822ab1255e2d
Nathan Burns <nate10nate@gmail.com>
2024-10-17 12:40:10 -0400

    Refactor fetch_weather.py to include weather API calls and location retrieval

--- src/utils/data/fetch_weather.py
+++ src/utils/data/fetch_weather.py
@@ -1,3 +1,31 @@
+import requests
+import os
+import geocoder
+
 def fetch_weather():
-    # Nathan: update with some sort of api call
-    return
+    weather_key = os.getenv('WEATHER_API_KEY')
+    location = geocoder.ip('me')
+    lat = location.latlng[0]
+    lon = location.latlng[1]
+    currentWeatherUrl = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_key}&units=imperial"
+    forcastWeatherUrl = f"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={weather_key}&lat={lat}&lon={lon}&units=imperial"
+    currentWeatherResponse = requests.get(currentWeatherUrl)
+    forcastResponse = requests.get(forcastWeatherUrl)
+    if currentWeatherResponse.status_code == 200 and forcastResponse.status_code == 200:
+        currentWeatherData = currentWeatherResponse.json()
+        forcastData = forcastResponse.json()
+        currentWeatherTemp = round(currentWeatherData['main']['temp'])
+        currentWeather = currentWeatherData['weather'][0]['main']
+        forcast = forcastData['list'][:6]
+        formattedForecast = [
+            {
+                "date": entry["dt_txt"],
+                "temp": round(entry["main"]["temp"]),
+                "weather": entry["weather"][0]["main"]
+            }
+            for entry in forcast
+        ]
+        return currentWeatherTemp, currentWeather, formattedForecast
+    else:
+        return ["Error fetching weather data"]
+    
